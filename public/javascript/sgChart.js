1ae96244c7cbe87643464f935c0ce1b235fe15df
jules <julianmackintosh@gmail.com>
2018-10-03 23:32:30 +0100

    Make chart scale better

--- public/javascript/sgChart.js
+++ public/javascript/sgChart.js
@@ -1,21 +1,37 @@
 
 
+
+
 function drawSgChart(target){
-  var ctx = document.getElementById(target);
+  const ctx = document.getElementById(target);
   $.ajax({
       url: '/ns/sgData',
       dataType: 'json',
     }).done(function (sgData) {
-      var myChart = new Chart(ctx, {
+      const minY = sgData.sgSamples.reduce((p, v) => Math.min(p, v));// * 0.90;
+      const maxY = sgData.sgSamples.reduce((p, v) => Math.max(p, v));// * 1.10;
+      const possibleTicks = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+      var smallTicks = possibleTicks.filter(x => x<=minY)
+      smallTicks.splice(-1,1);
+      var largeTicks = possibleTicks.filter(x => x>=maxY)
+      largeTicks.splice(0,1);
+      const ticksToRemove = new Set(smallTicks.concat(largeTicks));
+      const ticksY = possibleTicks.filter(x => !ticksToRemove.has(x))
+      const actualMinY = ticksY[0];
+      const actualMaxY = ticksY[ticksY.length-1];
+      const myChart = new Chart(ctx, {
           type: 'line',
           data: {
               labels: sgData.timeStamps.map(x =>timeString(new Date(x))),
               datasets: [{
                   label: 'Sensor Glucuse mmol/L',
                   data: sgData.sgSamples,
-                  borderColor: "#3e95cd",
+                  borderColor: "#ff0000",
+                  backgroundColor: "#ff0000",
                   borderWidth: 1,
-                  fill: true
+                  fill: false,
+                  pointRadius: 6,
+                  borderDash: [6,3]
               }]
           },
           options: {
@@ -23,16 +39,25 @@ function drawSgChart(target){
                 duration :0
               },
               scales: {
-                  yAxes: [{
-                      ticks: {
-                          beginAtZero:true
-                      }
-                  }],
                   xAxes: [{
                       ticks: {
                           display: true //this will remove only the label
                       }
-                  }]
+                  }],
+                  yAxes: [{
+          					display: true,
+          					type: 'logarithmic',
+                    ticks: {
+                      min: actualMinY,
+                      max: actualMaxY,
+                        callback: function(value, index, values) {//needed to change the scientific notation results from using logarithmic scale
+                            return Number(value.toString());//pass tick values as a string into Number function
+                        }
+                    },
+                    afterBuildTicks: function(pckBarChart) {
+                      pckBarChart.ticks = ticksY;
+                      }
+          				}]
               }
           }
       }
