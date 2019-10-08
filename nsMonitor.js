49ebf7220b209f7752fcce6d409f98cfb1b0f0b8
jules <julianmackintosh@gmail.com>
2019-10-08 20:54:04 +0100

    remove debuging log

--- nsMonitor.js
+++ nsMonitor.js
@@ -17,7 +17,7 @@ const nsMonitor = function (nightScout, logFactory){
   function periodicCheck() {
     nightScout.getSgData(3)
       .then(data =>{
-        log("debug:" + JSON.stringify(data));
+        //log("debug:" + JSON.stringify(data));
         log("sample:" + moment(new Date(data.lastTimestamp)).format("DD-MM HH:mm:ss") + ", average:" + data.mean.toFixed(1) + ", last: "+ data.sgSamples[2].toFixed(1));
         if (data.mean < config.thresholdLow)
           alarm.triggerAlarm(config.snoozeTimeForLow)
